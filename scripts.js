e2d05972d747540d38e52f0dd221fcd7684d31bc
Shakira D <4237135+shakira91@users.noreply.github.com>
2017-11-03 16:44:13 -0400

    things

--- scripts.js
+++ scripts.js
@@ -1,24 +1,45 @@
 "use strict";
 (() => {
 
-	function tracker(el, element) {
-		var countdown;
-		
-		for (var n = 50; n > 0; n--) {
-			var digit1 = element;
-			return (() => {
-				countdown = window.setInterval(() => { 
-					digit1.innerHTML = el +-- n;
-					if (element.innerHTML == el +'0') {
-						window.clearInterval(countdown);
-					}
-				}, 50);	
-			})(n);
-		}
+	function getData(region, month) {
+	    var starCountRef = firebase.database().ref(region + '/' + month +'/').on('value', function(snapshot) {
+		  console.log(snapshot.val()) // returns amount at specified point.
+		});
 	}
 
-	tracker(document.getElementById("input1").innerHTML, document.getElementById("input1"));
-	tracker(document.getElementById("input2").innerHTML, document.getElementById("input2"));
+	function setData(region, amount) {
+		firebase.database().ref(region + '/').child(document.getElementById("input1month").value).set({
+	    	amount : amount // sets data in Database
+	  });
+	}
+
+	document.getElementById("submit").addEventListener("click", () => {
+		getData('Africa', 'October');
+		setData('Africa', document.getElementById("input1amount").value);
+
+	});
+
+	// function tracker(el, element) {
+	// 	var countdown;
+	// 	var num = parseInt(el); //new number: 104 
+	// 	var prev = 316; 
+	// console.log(prev - num)
+	// 	for (var n = 0; n >= 0; n--) {
+	// 		return (() => {
+	// 			countdown = window.setInterval(() => {
+	// 				element.innerHTML = num--;
+					
+	// 				if (element.innerHTML == (prev).toString()) {
+						
+	// 					window.clearInterval(countdown);
+	// 				}
+	// 			}, 100);	
+	// 		})(n);
+	// 	}
+	// }
+
+	// tracker(document.getElementById("input1").innerHTML, document.getElementById("input1"));
+	// tracker(document.getElementById("input2").innerHTML, document.getElementById("input2"));
 
 })();
 
